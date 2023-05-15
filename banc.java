ee1bd9639f84719881b1dd1577f2ba7c76df07d7
Gustavo Adolfo Rodriguez <1432287+garodriguezlp@users.noreply.github.com>
2023-05-15 08:06:35 -0500

    Add new cli tool for transforming my financial data

---
+++ banc.java
@@ -0,0 +1,210 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+
+//JAVA 17+
+
+// Update the Quarkus version to what you want here or run jbang with
+// `-Dquarkus.version=<version>` to override it.
+//DEPS io.quarkus:quarkus-bom:${quarkus.version:2.11.2.Final}@pom
+//DEPS io.quarkus:quarkus-picocli
+//DEPS org.apache.commons:commons-csv:1.8
+
+//Q:CONFIG quarkus.banner.enabled=false
+//Q:CONFIG quarkus.log.level=INFO
+//Q:CONFIG quarkus.log.category."io.quarkus".level=ERROR
+
+import io.quarkus.logging.Log;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import picocli.CommandLine.ArgGroup;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+// -- ------------------------------------------------------------------------------------------------------------------
+// -- Driving Adapters
+// -- ------------------------------------------------------------------------------------------------------------------
+@Command(
+    name = "banc",
+    mixinStandardHelpOptions = true,
+    description = "Transforms financial data to my own format",
+    version = "banc 0.1",
+    showDefaultValues = true,
+    sortOptions = false,
+    headerHeading = "Usage:%n%n",
+    synopsisHeading = "%n",
+    descriptionHeading = "%nDescription:%n%n",
+    parameterListHeading = "%nParameters:%n",
+    optionListHeading = "%nOptions:%n",
+    commandListHeading = "%nCommands:%n",
+    footerHeading = "%n",
+    header = "Transforms financial data from one format to another"
+)
+public class banc implements Runnable {
+
+  @Parameters(index = "0", description = "The input file with finacial data")
+  private File inputFile;
+
+  @ArgGroup(exclusive = true, multiplicity = "1")
+  Institution institution;
+
+  static class Institution {
+    @Option(names = {"--bancolombia", "-b"}, required = true)
+    boolean bancolombia;
+
+    @Option(names = {"--peoplepass", "-p"}, required = true)
+    boolean peoplepass;
+  }
+
+  @Inject
+  TransformationService transformationService;
+
+  @Override
+  public void run() {
+    transformationService.map(inputFile, financialInstitution());
+  }
+
+  private FinancialInstitution financialInstitution() {
+    FinancialInstitution fInstitution = null;
+    if (institution.bancolombia) {
+      fInstitution = FinancialInstitution.BANCOLOMBIA;
+    } else if (institution.peoplepass) {
+      fInstitution = FinancialInstitution.PEOPLEPASS;
+    }
+    return fInstitution;
+  }
+
+}
+
+// -- ------------------------------------------------------------------------------------------------------------------
+// -- Driven Adapters
+// -- ------------------------------------------------------------------------------------------------------------------
+@ApplicationScoped
+class BancolombiaReader implements RecordReader<BancolombiaRecord> {
+
+  private static final Charset WINDOWS_1252 = Charset.forName("windows-1252");
+  private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+
+  enum Headers {
+    DATE, DOCUMENT, OFFICE, DESCRIPTION, REFERENCE, VALUE
+  }
+
+  @Override
+  public boolean supports(FinancialInstitution fInstitution) {
+    return fInstitution == FinancialInstitution.BANCOLOMBIA;
+  }
+
+  @Override
+  public List<BancolombiaRecord> read(File inputFile) {
+    Log.infov("Reading file {0} for {1}", inputFile, FinancialInstitution.BANCOLOMBIA);
+    return parseCvsFile(inputFile)
+        .stream()
+        .map(this::toBancolombiaRecord)
+        .toList();
+  }
+
+  private List<CSVRecord> parseCvsFile(File csvFile)  {
+    try (Reader reader = new InputStreamReader(new FileInputStream(csvFile), WINDOWS_1252)) {
+      return CSVFormat.TDF
+          .withHeader(Headers.class)
+          .withSkipHeaderRecord()
+          .withAllowMissingColumnNames()
+          .parse(reader)
+          .getRecords();
+    } catch (Exception e) {
+      Log.errorv(e, "Error reading file {0}", csvFile);
+      throw new RuntimeException(e);
+    }
+  }
+
+  private BancolombiaRecord toBancolombiaRecord(CSVRecord csvRecord) {
+    return new BancolombiaRecord(
+        LocalDate.parse(csvRecord.get(Headers.DATE), DATE_TIME_FORMATTER),
+        csvRecord.get(Headers.DESCRIPTION),
+        csvRecord.get(Headers.VALUE)
+    );
+  }
+}
+
+@ApplicationScoped
+class RecordReaderFactory {
+
+  @Any
+  @Inject
+  Instance<RecordReader<?>> recordReaders;
+
+  public <T> RecordReader<T> readerFor(FinancialInstitution fInstitution) {
+    return (RecordReader<T>) recordReaders.stream()
+        .filter(reader -> reader.supports(fInstitution))
+        .findFirst()
+        .orElseThrow(() -> new IllegalArgumentException("No reader found for " + fInstitution));
+  }
+}
+
+// -- ------------------------------------------------------------------------------------------------------------------
+// -- Ports
+// -- ------------------------------------------------------------------------------------------------------------------
+interface TransformationService{
+  <T> void map(File inputFile, FinancialInstitution fInstitution);
+}
+
+interface RecordReader<T> {
+  boolean supports(FinancialInstitution fInstitution);
+  List<T> read(File inputFile);
+}
+
+// -- ------------------------------------------------------------------------------------------------------------------
+// -- Domain
+// -- ------------------------------------------------------------------------------------------------------------------
+enum FinancialInstitution {
+  BANCOLOMBIA, PEOPLEPASS
+}
+
+record TargetRecord(
+    LocalDate date,
+    String description,
+    String amount) {
+}
+
+record BancolombiaRecord(
+    LocalDate date,
+    String description,
+    String amount) {
+}
+
+record PeoplePassRecord(
+    LocalDate date,
+    String description,
+    String amount) {
+}
+
+// -- ------------------------------------------------------------------------------------------------------------------
+// -- Application: Use Cases
+// -- ------------------------------------------------------------------------------------------------------------------
+@ApplicationScoped
+class TransformationServiceImpl implements TransformationService {
+
+  @Inject
+  RecordReaderFactory recordReaderFactory;
+
+  @Override
+  public <T> void map(File inputFile, FinancialInstitution fInstitution) {
+    Log.infov("Transforming file {0} for {1}", inputFile, fInstitution);
+
+    RecordReader<T> reader = recordReaderFactory.readerFor(fInstitution);
+    List<T> records = reader.read(inputFile);
+    records.forEach(System.out::println);
+  }
+}
